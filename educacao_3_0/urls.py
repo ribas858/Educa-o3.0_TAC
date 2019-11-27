6b8f0c044b04ea750fc309129b77c2642a8dc9b0
ribas858 <ribas858@gmail.com>
2019-11-27 08:11:56 -0300

    save

--- educacao_3_0/urls.py
+++ educacao_3_0/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from site_est.views import index, elements, generic, aula, web, edu, labs, lab01, lab02, lab03, lab04, lab05, lab06, lab07, lab08
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('elements/', elements),
+    path('generic/', generic),
+    path('aula/', aula),
+    path('web/', web),
+    path('edu/', edu),
+    path('labs/', labs),
+    path('lab01/', lab01),
+    path('lab02/', lab02),
+    path('lab03/', lab03),
+    path('lab04/', lab04),
+    path('lab05/', lab05),
+    path('lab06/', lab06),
+    path('lab07/', lab07),
+    path('lab08/', lab08)
 ]
--- site_est/views.py
+++ site_est/views.py
@@ -1,3 +1,83 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404
 
 # Create your views here.
+
+
+def index(request):
+    
+    return render(request, 'site_est/index.html')
+
+def elements(request):
+    
+    return render(request, 'site_est/elements.html')
+
+def generic(request):
+    
+    return render(request, 'site_est/generic.html')
+
+def aula(request):
+        
+    return render(request, 'site_est/aula.html')
+
+def web(request):
+        
+    return render(request, 'site_est/web.html')
+
+def edu(request):
+        
+    return render(request, 'site_est/edu.html')
+
+def labs(request):
+        
+    return render(request, 'site_est/labs.html')
+
+def lab01(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_yamin_fernanda.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab02(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_marina.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab03(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_diegod.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab04(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_patrick.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab05(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_joao.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab06(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_meu.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab07(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_cristiane.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+def lab08(request):
+    try:
+        return FileResponse(open('site_est/static/site_est/labs/lab_carolzinha.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+
+
